c4329e7400e2466e428615144bd69ab826ab3c3d
Rustam Zokirov <79042556+Rustam-Zokirov@users.noreply.github.com>
2021-05-02 21:58:02 +0500

    updated

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -4,11 +4,6 @@ use Illuminate\Database\Seeder;
 
 class PostTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         $post = new \App\Post([
