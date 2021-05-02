51aff98c1cb74b20c28360304f59f4f1d2b4dab3
Rustam Zokirov <79042556+Rustam-Zokirov@users.noreply.github.com>
2021-05-02 21:40:44 +0500

    Update index.blade.php

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,8 +9,8 @@
     @foreach($posts as $post)
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1 class="post-title">{{ $post['title'] }}</h1>
-            <p>{{ $post['content'] }}!</p>
+            <h1 class="post-title">{{ $post->title }}</h1>
+            <p>{{  $post->body }}!</p>
             <p><a href="{{ route('blog.post', ['id' => array_search($post, $posts)]) }}">Read more...</a></p>
         </div>
     </div>
