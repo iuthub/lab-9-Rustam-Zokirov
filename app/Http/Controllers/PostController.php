2d8c87269d96730172290812ce15317af616b80a
Rustam Zokirov <79042556+Rustam-Zokirov@users.noreply.github.com>
2021-05-02 21:52:41 +0500

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,66 +2,91 @@
 
 namespace App\Http\Controllers;
 
+use App\Like;
 use App\Post;
+use App\Tag;
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use Illuminate\Session\Store;
 
 class PostController extends Controller
 {
-    public function getIndex(Store $session)
+    
+    public function getIndex()
     {
-        $post = new Post();
-        $posts = $post->getPosts($session);
+        $posts = Post::orderBy('created_at', 'desc')->get();
         return view('blog.index', ['posts' => $posts]);
     }
 
-    public function getAdminIndex(Store $session)
+    public function getAdminIndex()
     {
-        $post = new Post();
-        $posts = $post->getPosts($session);
+        $posts = Post::orderBy('title', 'asc')->get();
         return view('admin.index', ['posts' => $posts]);
     }
 
-    public function getPost(Store $session, $id)
+    public function getPost($id)
     {
-        $post = new Post();
-        $post = $post->getPost($session, $id);
+        $post = Post::where('id', $id)->with('likes')->first();
         return view('blog.post', ['post' => $post]);
+    }
+
+    public function getLikePost($id)
+    {
+        $post = Post::where('id', $id)->first();
+        $like = new Like();
+        $post->likes()->save($like);
+        return redirect()->back();
     }
 
     public function getAdminCreate()
     {
-        return view('admin.create');
+        $tags = Tag::all();
+        return view('admin.create', ['tags' => $tags]);
     }
 
-    public function getAdminEdit(Store $session, $id)
+    public function getAdminEdit($id)
     {
-        $post = new Post();
-        $post = $post->getPost($session, $id);
-        return view('admin.edit', ['post' => $post, 'postId' => $id]);
+        $post = Post::find($id);
+        $tags = Tag::all();
+        return view('admin.edit', ['post' => $post, 'postId' => $id, 'tags' => $tags]);
     }
 
-    public function postAdminCreate(Store $session, Request $request)
+    public function postAdminCreate(Request $request)
     {
         $this->validate($request, [
             'title' => 'required|min:5',
             'content' => 'required|min:10'
         ]);
-        $post = new Post();
-        $post->addPost($session, $request->input('title'), $request->input('content'));
+        $post = new Post([
+            'title' => $request->input('title'),
+            'content' => $request->input('content')
+        ]);
+        $post->save();
+        $post->tags()->attach($request->input('tags') === null ? [] : $request->input('tags'));
+
         return redirect()->route('admin.index')->with('info', 'Post created, Title is: ' . $request->input('title'));
     }
 
-    public function postAdminUpdate(Store $session, Request $request)
+    public function postAdminUpdate(Request $request)
     {
         $this->validate($request, [
             'title' => 'required|min:5',
             'content' => 'required|min:10'
         ]);
-        $post = new Post();
-        $post->editPost($session, $request->input('id'), $request->input('title'), $request->input('content'));
+        $post = Post::find($request->input('id'));
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+        $post->tags()->sync($request->input('tags') === null ? [] : $request->input('tags'));
         return redirect()->route('admin.index')->with('info', 'Post edited, new Title is: ' . $request->input('title'));
     }
+
+    public function getAdminDelete($id)
+    {
+        $post = Post::find($id);
+        $post->likes()->delete();
+        $post->tags()->detach();
+        $post->delete();
+        return redirect()->route('admin.index')->with('info', 'Post deleted!');
+    }
 }
+
+
